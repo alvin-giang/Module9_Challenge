757d71ace83cff1bb3abf37f73c16a78906c4e50
alvin-giang <160292685+alvin-giang@users.noreply.github.com>
2024-05-25 08:30:18 +0800

    "edit data engineering

--- Data_engineering.sql
+++ Data_engineering.sql
@@ -1,34 +1,34 @@
 CREATE TABLE departments(
-	dept_no VARCHAR PRIMARY KEY NOT NULL,
-	dept_name VARCHAR NOT NULL
+	dept_no VARCHAR (30) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR (30) NOT NULL
 );
 
 CREATE TABLE titles (
-	title_id VARCHAR PRIMARY KEY NOT NULL,
-	title VARCHAR NOT NULL
+	title_id VARCHAR (30) PRIMARY KEY NOT NULL,
+	title VARCHAR (30) NOT NULL
 );
 
 CREATE TABLE employee (
 	emp_no INTEGER PRIMARY KEY NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
+	emp_title_id VARCHAR (30) NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
-	birth_date VARCHAR NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date VARCHAR NOT NULL
+	birth_date DATE NOT NULL,
+	first_name VARCHAR (30) NOT NULL,
+	last_name VARCHAR (30) NOT NULL,
+	sex VARCHAR (30) NOT NULL,
+	hire_date DATE NOT NULL
 );
 
 CREATE TABLE dept_emp(
 	emp_no INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employee (emp_no),
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR (30) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR (30) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	emp_no INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employee (emp_no),
